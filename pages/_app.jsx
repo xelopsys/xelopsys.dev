34ebf0a902c3e5bec098c5b178e2f7a2f8029ea4
Jamshid Jurakulov <46597388+xelopsys@users.noreply.github.com>
2022-07-11 16:54:09 +0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,21 +9,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-213502733-1`}
-      />
-
-      <Script strategy="lazyOnload" id="">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-213502733-1', {
-        page_path: window.location.pathname,
-      });
-          `}
-      </Script>
       <Component {...pageProps} />
     </>
   )
